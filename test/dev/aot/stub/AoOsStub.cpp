896adfecbafeb104b7bedd888e1f6d2c4c7500de
PrQiang <71695576+PrQiang@users.noreply.github.com>
2020-09-27 16:47:11 +0800

    v2
    移除准备重建

--- test/dev/aot/stub/AoOsStub.cpp
+++
@@ -1,5 +0,0 @@
-#include "../../../../src/dev/util/AoOs.h"
-CAoOs::CAoOs() {}
-CAoOs::~CAoOs() {}
-int CAoOs::SysType() { return EN_OS_TYPE_WINDOWS; }
-void CAoOs::Sleep(int ) {}
--- test/dev/aot/stub/AoUnzipStub.cpp
+++
@@ -1,8 +0,0 @@
-#include "../../../../src/dev/util/AoUnzip.h"
-CAoUnzip::CAoUnzip() {}// 构造函数    
-CAoUnzip::~CAoUnzip() {}// 析构函数
-int CAoUnzip::OpenZipFile(const char* ) { return 0; }
-int CAoUnzip::GotoFirstFile() { return 0; }
-int CAoUnzip::GotoNextFile() { return 0; }
-int CAoUnzip::GetCurrentFileInfo(char* , int , bool& ) { return 0; }
-int CAoUnzip::UnzipCurentFile(CAoStream* , const char* ) { return 0; }
