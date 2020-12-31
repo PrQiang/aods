869338c0cd05ed3ea4e8b82e37c6dfabb7604c7e
PrQiang <71695576+PrQiang@users.noreply.github.com>
2020-12-31 15:38:29 +0800

    Revert "调整exaple和说明文档"
    This reverts commit 3bc25a66b3b904becdfa1718cfad6941139f9182.

--- example/PrjRestfulApiService/RestfulApiService.py
+++
@@ -1,7 +0,0 @@
-import socketserver, threading
-from RestfulApiHandler import RestfulApiHandler
-
-if __name__ == "__main__":
-    server = socketserver.ThreadingTCPServer(('0.0.0.0', 32002), RestfulApiHandler)
-    t = threading.Thread(target= server.serve_forever, args = ())
-    t.start()
